7d47f1d83120663243a1c96488dbdab60dae9acd
Irina <eira@mail.ru>
2023-02-23 13:20:40 +0100

    css and html template are in separate files

--- app/transpose_output.py
+++ app/transpose_output.py
@@ -20,95 +20,13 @@ def transpose_output(transposed_pattern_list: List[PatternInScale]) -> int:
         transposed_pattern_html = create_transposed_pattern_html(pattern_in_scale)
 
         title = f'{scale_type_name}, {pattern_name}'
-        plain_css = """html {
-    position: relative;
-    min-height: 100%;
-}
-
-body {
-    test-align: center;
-}
-
-h1 {
-    position: absolute;
-    top: 10px;
-    right: 15px;
-
-    font-size: 16px;
-    font-weight: bold;
-    color: lightgrey;
-}
-
-.pattern_name {
-    font-size: 28px;
-    margin-bottom: 5px;
-}
-
-.scale_group_name {
-    margin-top: 5px;
-    margin-bottom: 38px;
-}
-
-footer {
-    position: absolute;
-    right: 15px;
-    bottom: 10px;
-
-    color: grey;
-}
-
-.header {
-    text-align: center;
-}
-.content {
-    position: relative;
-    margin: auto;
-    text-align: center;
-}
-
-section {
-    display: inline-block;
-    min-width: max-content;
-    width: 48%;
-    margin-right: 10px;
-    margin-left: 10px;
-}
-
-.pattern {
-    font-size: 25px;
-}
-
-.scale_quant {
-    margin: 5px;
-}
-        """
-        html_code = """<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>{title}</title>
-        <style type="text/css">
-            {styles}
-        </style>
-    </head>
-    <body>
-        <header class="header">
-            <h1>into all scales</h1>
-            <h2 class="pattern_name">{pattern}</h2>
-            <h2 class="scale_group_name">{scale_group}</h2>
-        </header>
-        <div class="content">
-            {data}
-        </div>
-        <footer>©"Into all scales" created by Irina Eiduk, 2023</footer>
-    </body>
-</html>""".format(
-            pattern=pattern_name,
-            scale_group=scale_type_name,
-            title=title,
-            styles=plain_css,
-            data=transposed_pattern_html,
-        )
+        with open('../assets/page_template.html') as html_file:
+            html_code = html_file.read().format(
+                pattern=pattern_name,
+                scale_group=scale_type_name,
+                title=title,
+                data=transposed_pattern_html,
+            )
 
         file_name = f"results/{scale_type_name.replace(' ', '_').lower()}_{pattern_name.replace(' ', '_').lower()}"
 
